5a206e1eaa7996e7dc897d3abd0d6bb79d11453e
Laura <laura.e.vollmer@gmail.com>
2024-07-09 12:32:57 -0500

    styling mobile learn more button and client images

--- src/App.jsx
+++ src/App.jsx
@@ -22,21 +22,21 @@ function App() {
       <div>
         <img src={backgroundImageMobile} alt="background"></img>
       </div>
-      <div>
+      <div className="flex flex-col">
         <h1>Make remote work</h1>
         <h2>
           Get your team in sync, no matter your location. Streamline processes,
           create team rituals, and watch productivity soar.
         </h2>
       </div>
-      <div>
-        <button>Learn more</button>
+      <div className="flex items-center justify-center">
+        <button className="bg-almost-black text-almost-white font-bold rounded-lg p-2">Learn more</button>
       </div>
-      <div>
-        <img src={clientDatabiz} alt="Client Databiz image"></img>
-        <img src={clientAudiophile} alt="Client Audiophile image"></img>
-        <img src={clientMeet} alt="Client Meet image"></img>
-        <img src={clientMaker} alt="Client Maker image"></img>
+      <div className="flex flex-row items-center justify-between">
+        <img src={clientDatabiz} alt="Client Databiz image" className="h-4"></img>
+        <img src={clientAudiophile} alt="Client Audiophile image" className="h-4"></img>
+        <img src={clientMeet} alt="Client Meet image" className="h-4"></img>
+        <img src={clientMaker} alt="Client Maker image" className="h-4"></img>
       </div>
     </>
   );
