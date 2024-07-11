77453cf6f17d65d9d1806c5ca87e7f02d669d980
Laura <laura.e.vollmer@gmail.com>
2024-07-11 09:33:29 -0500

    debugging styling of navbaR

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
     <>
       <div className="space-y-4 bg-almost-white min-h-screen">
         {/* Header */}
-        <div className="flex flex-row items-center justify-between pt-6 pl-6 pr-6">
+        <div className="flex flex-row items-center">
           <div className="mobile-logo">
             <img src={logo} alt="Snap logo" />
           </div>
@@ -185,149 +185,157 @@ function App() {
             ></img>
           </div>
         </div>
+
         {/* Desktop Layout */}
         {/* Desktop Copy Text */}
-        <div className="flex p-8 items-center justify-between font-bold text-md font-epilogue">
-          <nav className="fixed top-0 left-0 items-center justify-between">
-            <div className="flex flex-row items-center space-x-20">
-              <img src={logo} alt="Snap logo" className="ml-10" />
-              <div
-                className="relative"
-                onClick={() => setIsFeaturesOpen(!isFeaturesOpen)}
-              >
-                <div className="flex flex-row items-center">
-                  <h2 className="text-medium-gray cursor-pointer">Features</h2>
-                  <img
-                    src={isFeaturesOpen ? upArrow : downArrow}
-                    alt={isFeaturesOpen ? "Up Arrow Icon" : "Down Arrow Icon"}
-                    className="w-4 h-2"
-                  />
-                </div>
-                {isFeaturesOpen && (
-                  <div className="absolute z-10 mt-1 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                    <ul
-                      className="py-2 text-sm text-gray-700 dark:text-gray-200 m-2"
-                      aria-labelledby="dropdownDefaultButton"
-                    >
-                      <li className="flex flex-row items-center space-x-2">
-                        <img src={todo} alt="todo icon" className="h-4 w-4" />
-                        <a
-                          href="#"
-                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-medium-gray"
-                        >
-                          Todo List
-                        </a>
-                      </li>
-                      <li className="flex flex-row items-center space-x-2">
-                        <img
-                          src={calendar}
-                          alt="todo icon"
-                          className="h-4 w-4"
-                        />
-                        <a
-                          href="#"
-                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-medium-gray"
-                        >
-                          Calendar
-                        </a>
-                      </li>
-                      <li className="flex flex-row items-center space-x-2">
-                        <img
-                          src={reminders}
-                          alt="todo icon"
-                          className="h-4 w-4"
-                        />
-                        <a
-                          href="#"
-                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-medium-gray"
-                        >
-                          Reminders
-                        </a>
-                      </li>
-                      <li className="flex flex-row items-center space-x-2">
-                        <img
-                          src={planning}
-                          alt="todo icon"
-                          className="h-4 w-4"
-                        />
-                        <a
-                          href="#"
-                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-medium-gray"
-                        >
-                          Planning
-                        </a>
-                      </li>
-                    </ul>
+        <div className="fixed top-0 left-0 right-0 w-full z-50 bg-white shadow-md p-10">
+          <div className="flex flex-row items-center text-md font-epilogue">
+            <img src={logo} alt="Snap logo" />
+            <div className="flex justify-between">
+              <div className="flex items-center space-x-10 ml-10">
+                <div
+                  className="relative"
+                  onClick={() => setIsFeaturesOpen(!isFeaturesOpen)}
+                >
+                  <div className="flex flex-row items-center">
+                    <h2 className="text-medium-gray cursor-pointer">
+                      Features
+                    </h2>
+                    <img
+                      src={isFeaturesOpen ? upArrow : downArrow}
+                      alt={isFeaturesOpen ? "Up Arrow Icon" : "Down Arrow Icon"}
+                      className="w-4 h-2"
+                    />
                   </div>
-                )}
-              </div>
-              <div
-                className="relative"
-                onClick={() => setIsCompanyOpen(!isCompanyOpen)}
-              >
-                <div className="flex flex-row items-center">
-                  <h2 className="text-medium-gray cursor-pointer">Company</h2>
-                  <img
-                    src={isCompanyOpen ? upArrow : downArrow}
-                    alt={isCompanyOpen ? "Up Arrow Icon" : "Down Arrow Icon"}
-                    className="w-4 h-2"
-                  ></img>
+                  {isFeaturesOpen && (
+                    <div className="absolute z-10 mt-1 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-medium-gray">
+                      <ul
+                        className="py-2 text-sm text-medium-gray dark:text-medium-gray m-2"
+                        aria-labelledby="dropdownDefaultButton"
+                      >
+                        <li className="flex flex-row items-center space-x-2">
+                          <img src={todo} alt="todo icon" className="h-4 w-4" />
+                          <a
+                            href="#"
+                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-medium-gray"
+                          >
+                            Todo List
+                          </a>
+                        </li>
+                        <li className="flex flex-row items-center space-x-2">
+                          <img
+                            src={calendar}
+                            alt="todo icon"
+                            className="h-4 w-4"
+                          />
+                          <a
+                            href="#"
+                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-medium-gray"
+                          >
+                            Calendar
+                          </a>
+                        </li>
+                        <li className="flex flex-row items-center space-x-2">
+                          <img
+                            src={reminders}
+                            alt="todo icon"
+                            className="h-4 w-4"
+                          />
+                          <a
+                            href="#"
+                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-medium-gray"
+                          >
+                            Reminders
+                          </a>
+                        </li>
+                        <li className="flex flex-row items-center space-x-2">
+                          <img
+                            src={planning}
+                            alt="todo icon"
+                            className="h-4 w-4"
+                          />
+                          <a
+                            href="#"
+                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-medium-gray"
+                          >
+                            Planning
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  )}
                 </div>
-                {isCompanyOpen && (
-                  <div className="absolute z-10 mt-1 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                    <ul
-                      className="py-2 text-sm text-gray-700 dark:text-gray-200 m-2"
-                      aria-labelledby="dropdownDefaultButton"
-                    >
-                      <li className="flex flex-row items-center space-x-2">
-                        <a
-                          href="#"
-                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-medium-gray"
-                        >
-                          History
-                        </a>
-                      </li>
-                      <li className="flex flex-row items-center space-x-2">
-                        <a
-                          href="#"
-                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-medium-gray"
-                        >
-                          Team
-                        </a>
-                      </li>
-                      <li className="flex flex-row items-center space-x-2">
-                        <a
-                          href="#"
-                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-medium-gray"
-                        >
-                          Blog
-                        </a>
-                      </li>
-                    </ul>
+                <div
+                  className="relative"
+                  onClick={() => setIsCompanyOpen(!isCompanyOpen)}
+                >
+                  <div className="flex flex-row items-center">
+                    <h2 className="text-medium-gray cursor-pointer">Company</h2>
+                    <img
+                      src={isCompanyOpen ? upArrow : downArrow}
+                      alt={isCompanyOpen ? "Up Arrow Icon" : "Down Arrow Icon"}
+                      className="w-4 h-2"
+                    ></img>
                   </div>
-                )}
-              </div>
-              <div className="flex flex-row space-x-2">
-                <a href="#" className="text-medium-gray">
-                  Careers
-                </a>
-              </div>
+                  {isCompanyOpen && (
+                    <div className="absolute z-10 mt-1 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-medium-gray">
+                      <ul
+                        className="py-2 text-sm text-medium-gray dark:text-medium-gray m-2"
+                        aria-labelledby="dropdownDefaultButton"
+                      >
+                        <li className="flex flex-row items-center space-x-2">
+                          <a
+                            href="#"
+                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-medium-gray"
+                          >
+                            History
+                          </a>
+                        </li>
+                        <li className="flex flex-row items-center space-x-2">
+                          <a
+                            href="#"
+                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-medium-gray"
+                          >
+                            Team
+                          </a>
+                        </li>
+                        <li className="flex flex-row items-center space-x-2">
+                          <a
+                            href="#"
+                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-medium-gray"
+                          >
+                            Blog
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  )}
+                </div>
 
-              <div className="flex flex-row space-x-2">
-                <a href="#" className="text-medium-gray">
-                  About
-                </a>
+                <div className="flex flex-row space-x-2">
+                  <a href="#" className="text-medium-gray">
+                    Careers
+                  </a>
+                </div>
+
+                <div className="flex flex-row space-x-2">
+                  <a href="#" className="text-medium-gray">
+                    About
+                  </a>
+                </div>
               </div>
-              <div className="flex flex-row p-6 space-x-10 font-md font-epilogue">
-                <button className="bg-almost-white text-medium-gray rounded-lg p-2">
-                  Login
-                </button>
-                <button className="bg-almost-white text-medium-gray rounded-lg p-2 border border-medium-gray">
-                  Register
-                </button>
+              <div>
+                <div className="flex flex-row p-6 space-x-10 font-md font-epilogue">
+                  <button className="bg-almost-white text-medium-gray rounded-lg p-2">
+                    Login
+                  </button>
+                  <button className="bg-almost-white text-medium-gray rounded-lg p-2 border border-medium-gray">
+                    Register
+                  </button>
+                </div>
               </div>
             </div>
-          </nav>
+          </div>
         </div>
         <div className="grid-container">
           <div className="flex flex-col space-y-20 justify-start p-20 grid-item">
